4a7085ab1aef69e49654ce2de8748b598d001f7e
Steminists16556 <106121445+Steminists16556@users.noreply.github.com>
2022-09-10 15:23:14 -0500

    Update Morticia.java

--- Morticia.java
+++ Morticia.java
@@ -8,13 +8,12 @@ import com.qualcomm.robotcore.hardware.Servo;
 @TeleOp(name = "Morticia (Blocks to Java)")
 public class Morticia extends LinearOpMode {
 
-  //private Servo armServo;
-  private DcMotor frontright;
-  private DcMotor frontleft;
-  private DcMotor backright;
-  private DcMotor backleft;
-  //private DcMotor armMotor;
-  //private DcMotor carouselMotor;
+  
+  private DcMotor frontRight;
+  private DcMotor frontLeft;
+  private DcMotor backRight;
+  private DcMotor backLeft;
+  
 
   /**
    * This function is executed when this Op Mode is selected from the Driver Station.
@@ -23,62 +22,56 @@ public class Morticia extends LinearOpMode {
   public void runOpMode() {
     float targetPower2;
 
-    armServo = hardwareMap.get(Servo.class, "armServo");
-    frontright = hardwareMap.get(DcMotor.class, "front right");
-    frontleft = hardwareMap.get(DcMotor.class, "front left");
-    backright = hardwareMap.get(DcMotor.class, "back right");
-    backleft = hardwareMap.get(DcMotor.class, "back left");
-    //armMotor = hardwareMap.get(DcMotor.class, "armMotor");
-    //carouselMotor = hardwareMap.get(DcMotor.class, "carouselMotor");
-
+    
+    frontRight = hardwareMap.get(DcMotor.class, "frontRight");
+    frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
+    backRight = hardwareMap.get(DcMotor.class, "backRight");
+    backLeft = hardwareMap.get(DcMotor.class, "backLeft");
+    
     waitForStart();
-    armServo.setPosition(1);
+    
     if (opModeIsActive()) {
       // Put run blocks here.
       while (opModeIsActive()) {
         // forward and backwards
+        //frt
         targetPower2 = gamepad1.left_stick_y;
-        frontright.setPower(targetPower2);
-        targetPower2 = -gamepad1.left_stick_y;
-        frontleft.setPower(targetPower2);
-        targetPower2 = -gamepad1.left_stick_y;
-        backright.setPower(targetPower2);
+        frontRight.setPower(targetPower2);
         targetPower2 = gamepad1.left_stick_y;
-        backleft.setPower(targetPower2);
+        frontLeft.setPower(targetPower2);
+        //bck
+        targetPower2 = gamepad1.left_stick_y;
+        backRight.setPower(targetPower2);
+        targetPower2 = -gamepad1.left_stick_y;
+        backLeft.setPower(targetPower2);
         telemetry.update();
+        
+        
         // left and right
-        targetPower2 = gamepad1.right_stick_x;
-        frontright.setPower(targetPower2);
-        targetPower2 = gamepad1.right_stick_x;
-        frontleft.setPower(targetPower2);
-        targetPower2 = gamepad1.right_stick_x;
-        backright.setPower(targetPower2);
-        targetPower2 = gamepad1.right_stick_x;
-        backleft.setPower(targetPower2);
+        //frt
+        targetPower2 = gamepad1.left_stick_x;
+        frontRight.setPower(targetPower2);
+        targetPower2 = -gamepad1.left_stick_x;
+        frontLeft.setPower(targetPower2);
+        //bck
+        targetPower2 = -gamepad1.left_stick_x;
+        backRight.setPower(targetPower2);
+        targetPower2 = -gamepad1.left_stick_x;
+        backLeft.setPower(targetPower2);
         telemetry.update();
+        
         // turning
-        targetPower2 = -gamepad1.left_stick_x;
-        frontright.setPower(targetPower2);
-        targetPower2 = -gamepad1.left_stick_x;
-        frontleft.setPower(targetPower2);
-        targetPower2 = gamepad1.left_stick_x;
-        backright.setPower(targetPower2);
-        targetPower2 = gamepad1.left_stick_x;
-        backleft.setPower(targetPower2);
-        // Carousel Motor 
-        //targetPower2 = gamepad2.right_stick_y;
-        //carouselMotor.setPower(targetPower2);
-        // arm motor
-        /**targetPower2 = gamepad2.left_stick_y / 2;
-        armMotor.setPower(targetPower2);
-        // servor
-        if (gamepad2.a) {
-          armServo.setPosition(0);
-        } else {
-          armServo.setPosition(0.5);
-        }    */
-        // idk
+        //frt
+        targetPower2 = gamepad1.right_stick_x;
+        frontRight.setPower(targetPower2);
+        targetPower2 = -gamepad1.right_stick_x;
+        frontLeft.setPower(targetPower2);
+        //bck
+        targetPower2 = gamepad1.right_stick_x;
+        backRight.setPower(targetPower2);
+        targetPower2 = gamepad1.right_stick_x;
+        backLeft.setPower(targetPower2);
+
       }
     }
   }
-}
